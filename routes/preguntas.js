a6cd37d80a29ec3749960d5fb70145edafda89a9
GomezLuque <69279682+GomezLuque@users.noreply.github.com>
2021-05-22 18:09:23 -0500

    Delete routes directory

--- routes/preguntas.js
+++
@@ -1,9 +0,0 @@
-const routerx = require('express-promise-router');
-const preguntasController = require('../controllers/PreguntasController');
-
-const router = routerx();
-
-router.get('/list', preguntasController.listar);
-router.get('/list/:preguntaId', preguntasController.listar2 )
-
-module.exports = router;
--- routes/respuestas.js
+++
@@ -1,8 +0,0 @@
-const routerx = require('express-promise-router');
-const respuestasController = require('../controllers/RespuestasController');
-
-const router = routerx();
-
-router.get('/list', respuestasController.listar);
-
-module.exports = router;
